309f4299c8d6348f8da696ab2fc2963229091f94
laurennc <lcorlies@gmail.com>
2013-10-03 18:40:16 -0400

    taking Projection steps

--- initial_plots.py
+++ initial_plots.py
@@ -1,10 +1,25 @@
-
 from yt.mods import *
 
-fn="r0058_l10/redshift0058"
+fn="/Users/laurennc/data/RyanSims/r0058_l10/redshift0058"
 
 pf = load(fn, file_style="%s.grid.cpu%%04i") # load data
 
-SlicePlot(pf, 'x', "Density", width = (800.0, 'kpc')).save()
-SlicePlot(pf, 'y', "Density", width = (800.0, 'kpc')).save()
-SlicePlot(pf, 'z', "Density", width = (800.0, 'kpc')).save()
+#what types of plots do I want to make?  Density projections, and HI projections that are all the same size, color maps, and limits as Ximena's data!
+
+HI_Density
+
+fields = {'Density','HHHH'}
+zlim = {"Density": (),
+	 "": ()}
+
+#Need to find the position of the maximum density!
+val, pos = pf.h.find_max('Density')
+
+rad = 108.0/pf['kpc']
+
+pp = ProjectionPlot(pf,'z',fields,center=pos,width=(108.,'kpc'),axes_unit=['kpc','kpc'])
+
+pp.set_zlim(14.0,23.5)
+pp.set_cmap('spectral')
+
+#need to learn how to make off-axis projections.  Practice by making an edge-on and face-on disk for comparison with Ximena!
