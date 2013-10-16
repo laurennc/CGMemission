02617b266ba4f294eab5a3d738f49e3d50629264
laurennc <lcorlie@gmail.com>
2013-10-16 13:20:23 -0400

    basic projection code

---
+++ basic_projection.py
@@ -0,0 +1,14 @@
+from lauren import *
+
+fn="/hpc/astrostats/astro/users/lnc2115/Ryan/r0058_l10/redshift0058"
+
+pf = load(fn, file_style="%s.grid.cpu%%04i") # load data
+
+pos = [0.40328598,0.47176743,0.46131516]
+
+rad = 108.0/pf['kpc']
+
+pp = ProjectionPlot(pf,'x','EmissionMeasureCold',center=pos,width=(108.,'kpc'),axes_unit=['kpc','kpc'])
+
+pp.set_cmap('all','spectral')
+pp.save()
