66111356fece52825bb5a6ac78ccdcd7250a5be3
laurennc <lcorlie@gmail.com>
2013-10-26 23:18:02 -0400

    updated to make H Alpha projections from Cloudy data

--- basic_projection.py
+++ basic_projection.py
@@ -5,10 +5,11 @@ fn="/hpc/astrostats/astro/users/lnc2115/Ryan/r0058_l10/redshift0058"
 pf = load(fn, file_style="%s.grid.cpu%%04i") # load data
 
 pos = [0.40328598,0.47176743,0.46131516]
-
 rad = 108.0/pf['kpc']
 
-pp = ProjectionPlot(pf,'x','EmissionMeasureCold',center=pos,width=(108.,'kpc'),axes_unit=['kpc','kpc'])
+field = ['HAlpha_Emissivity2']
+
+pp = ProjectionPlot(pf,'x',field,center=pos,width=(108.,'kpc'),axes_unit=['kpc','kpc'])
 
 pp.set_cmap('all','spectral')
 pp.save()
