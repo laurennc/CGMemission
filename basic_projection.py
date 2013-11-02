b778f7409475f7b66d0cd548877e7b438bed88dc
laurennc <lcorlie@gmail.com>
2013-11-02 15:11:34 -0400

    different projections with set limits

--- basic_projection.py
+++ basic_projection.py
@@ -7,9 +7,12 @@ pf = load(fn, file_style="%s.grid.cpu%%04i") # load data
 pos = [0.40328598,0.47176743,0.46131516]
 rad = 108.0/pf['kpc']
 
-field = ['HAlpha_Emissivity2']
+field = ['HAlpha_Voort_R','HAlpha_Emissivity_R']
 
-pp = ProjectionPlot(pf,'x',field,center=pos,width=(108.,'kpc'),axes_unit=['kpc','kpc'])
+
+pp = ProjectionPlot(pf,'y',field,center=pos,width=(108.,'kpc'),axes_unit=['kpc','kpc'])
 
 pp.set_cmap('all','spectral')
+pp.set_zlim('all',10**-5.0,10**1.0)
+#pp.set_zlim('all',10**14.0,10**23.5)
 pp.save()
