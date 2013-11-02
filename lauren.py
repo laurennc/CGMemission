5ceb912cb59550d9a4d162b5239ecb4e84c954cd
laurennc <lcorlie@gmail.com>
2013-11-02 15:11:14 -0400

    added code to plot the top percent of particles above the mean

--- lauren.py
+++ lauren.py
@@ -1,5 +1,6 @@
 from yt.mods import *
 import numpy as np
+import matplotlib.pyplot as plt
 
 def ergs_sr_TO_raleighs(data_arr):
 	return data_arr*3.304e11/79577.4715459
@@ -7,19 +8,37 @@ def ergs_sr_TO_raleighs(data_arr):
 def distance_from_center(x,y,z,center):
         return ((x-center[0])**2.0+(y-center[1])**2.0+(z-center[2])**2.0)**0.5
 
-#def make_plots(xs,labels=None, **kwargs):
-#	K = len(xs)
-#	factor = 2.0 # size of one side of one panel
-#	lbdim = 0.5 * factor # size of left/bottom margin
-#	trdim = 0.05 * factor # size of top/right margin
-#	whspace = 0.05 # w/hspace size
-#	plotdim = factor * K + factor * (K - 1.) * whspace
-#	dim = lbdim + plotdim + trdim
-#
-#	fig, axes = plt.subplots(3, 6, figsize=(dim, dim))
-#
-#	lb = lbdim / dim
-#	tr = (lbdim + plotdim) / dim
-#	fig.subplots_adjust(left=lb, bottom=lb, right=tr, top=tr,
-#                       wspace=whspace, hspace=whspace)
-#
+def HAlpha_Emission_Arc(t,hden):
+	#assuming that n_e/n_H = 1.15 and n_HII/n_H = 0.9936
+	Tfour = (10**t)/1.0e4
+	exponent = -0.942-0.031*np.log(Tfour)
+	return (2.82e-26)*(1.15*0.9936)*(2.3528e-11)*((10**hden)**2.0)*Tfour**(exponent)
+
+def plot_scatter_percentile(data,x,y,percentile):
+	x,y = np.meshgrid(x,y)
+	working_mask = np.ones(data.shape,bool)
+	r = abs(x+1j*y)
+	rmax = r[working_mask].max()
+        dr = np.abs([x[0,0] - x[0,1]]) #* annulus_width
+        radial = np.arange(rmax/dr)*dr + dr/2.
+        nrad = len(radial)	
+
+#	print 'nrad is',nrad	
+
+	for irad in range(nrad):
+		minrad = irad*dr
+		maxrad = minrad + dr
+		thisindex = (r>=minrad) * (r<maxrad) * working_mask
+		rhere = r[thisindex].flatten()
+		datahere = data[thisindex].flatten()
+		meanhere = data[thisindex].mean()
+		idx = np.where(datahere > meanhere)[0]
+		lenperc = int(len(idx)*percentile)
+		idSort = np.argsort(datahere)[::-1]
+		wanted = idSort[0:lenperc]
+		if len(wanted) == 0:
+			wanted = np.where(datahere == datahere.max())
+#		print wanted
+		plt.plot(rhere[wanted],datahere[wanted],'go')#,markersize=0.75)				
+
+
