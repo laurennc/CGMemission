3f04352a0cfa0e28a3a8123dc220f501afa1aed2
laurennc <lcorlie@gmail.com>
2013-11-15 17:22:12 -0500

    added code to load Ryans simulation and create the data sphere that I typically use

--- lauren.py
+++ lauren.py
@@ -8,6 +8,15 @@ def ergs_sr_TO_raleighs(data_arr):
 def distance_from_center(x,y,z,center):
         return ((x-center[0])**2.0+(y-center[1])**2.0+(z-center[2])**2.0)**0.5
 
+def load_Ryan_data():
+	fn="/hpc/astrostats/astro/users/lnc2115/Ryan/r0058_l10/redshift0058"
+	pf = load(fn, file_style="%s.grid.cpu%%04i") # load data
+	pos = [0.40328598,0.47176743,0.46131516]
+	rad = 108.0/pf['kpc']
+	data = pf.h.sphere(pos,rad)
+	return pf, data
+
+
 def HAlpha_Emission_Arc(t,hden):
 	#assuming that n_e/n_H = 1.15 and n_HII/n_H = 0.9936
 	Tfour = (10**t)/1.0e4
@@ -23,8 +32,6 @@ def plot_scatter_percentile(data,x,y,percentile):
         radial = np.arange(rmax/dr)*dr + dr/2.
         nrad = len(radial)	
 
-#	print 'nrad is',nrad	
-
 	for irad in range(nrad):
 		minrad = irad*dr
 		maxrad = minrad + dr
@@ -37,7 +44,6 @@ def plot_scatter_percentile(data,x,y,percentile):
 		wanted = idSort[0:lenperc]
 		if len(wanted) == 0:
 			wanted = np.where(datahere == datahere.max())
-#		print wanted
 		plt.plot(rhere[wanted],np.log10(datahere[wanted]),'go',markersize=3.5)#,markersize=0.75)				
 
 
