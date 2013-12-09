f8b8f6042b4404a8c1c3af3d3457111120923d57
laurennc <lcorlie@gmail.com>
2013-12-09 09:27:02 -0500

    changed to make pickles of the new metal line emission fields

--- make_frb_files.py
+++ make_frb_files.py
@@ -1,12 +1,15 @@
 from lauren import *
 import cPickle
 
-fn="/hpc/astrostats/astro/users/lnc2115/Ryan/r0058_l10/redshift0058"
+fn="/u/10/l/lnc2115/vega/data/Ryan/r0058_l10/redshift0058"
 pf = load(fn, file_style="%s.grid.cpu%%04i") # load data
 
-fields = ["HAlpha_Emissivity_R","Emission_CIV","Emission_OVI"]
+#fields = ["HAlpha_Emissivity_R","Emission_CIV","Emission_OVI"]
+fields = ["Emission_CIV","Emission_OVI","Emission_CIV_Scaled","Emission_OVI_Scaled","Emission_CIV_Scaled_ncut","Emission_OVI_Scaled_ncut"]
+
 width = 200./pf['kpc']
-res = [800,800]
+#res = [800,800]
+res = [40,40]
 pos = [0.40328598,0.47176743,0.46131516]
 dims = "xyz"
 
@@ -19,29 +22,48 @@ frby = projy.to_frb(width,res,center=pos)
 projz = pf.h.proj(2,fields)
 frbz = projz.to_frb(width,res,center=pos)
 
-fileout = 'frbx_250pc_Ha.cpkl'
-cPickle.dump(frbx['HAlpha_Emissivity_R'],open(fileout,'wb'),protocol=-1)
-fileout = 'frby_250pc_Ha.cpkl'
-cPickle.dump(frby['HAlpha_Emissivity_R'],open(fileout,'wb'),protocol=-1)
-fileout = 'frbz_250pc_Ha.cpkl'
-cPickle.dump(frbz['HAlpha_Emissivity_R'],open(fileout,'wb'),protocol=-1)
-
-fileout = 'frbx_250pc_CIV.cpkl'
+fileout = 'frbx_5kpc_OVI.cpkl'
 cPickle.dump(frbx['Emission_CIV'],open(fileout,'wb'),protocol=-1)
-fileout = 'frby_250pc_CIV.cpkl'
+fileout = 'frby_5kpc_Ha.cpkl'
 cPickle.dump(frby['Emission_CIV'],open(fileout,'wb'),protocol=-1)
-fileout = 'frbz_250pc_CIV.cpkl'
+fileout = 'frbz_5kpc_Ha.cpkl'
 cPickle.dump(frbz['Emission_CIV'],open(fileout,'wb'),protocol=-1)
 
-fileout = 'frbx_250pc_OVI.cpkl'
+fileout = 'frbx_5kpc_CIV_Scaled.cpkl'
+cPickle.dump(frbx['Emission_CIV_Scaled'],open(fileout,'wb'),protocol=-1)
+fileout = 'frby_5kpc_CIV_Scaled.cpkl'
+cPickle.dump(frby['Emission_CIV_Scaled'],open(fileout,'wb'),protocol=-1)
+fileout = 'frbz_5kpc_CIV_Scaled.cpkl'
+cPickle.dump(frbz['Emission_CIV_Scaled'],open(fileout,'wb'),protocol=-1)
+
+fileout = 'frbx_5kpc_CIV_Scaled_ncut.cpkl'
+cPickle.dump(frbx['Emission_CIV_Scaled_ncut'],open(fileout,'wb'),protocol=-1)
+fileout = 'frby_5kpc_CIV_Scaled_ncut.cpkl'
+cPickle.dump(frby['Emission_CIV_Scaled_ncut'],open(fileout,'wb'),protocol=-1)
+fileout = 'frbz_5kpc_CIV_Scaled_ncut.cpkl'
+cPickle.dump(frbz['Emission_CIV_Scaled_ncut'],open(fileout,'wb'),protocol=-1)
+
+
+fileout = 'frbx_5kpc_OVI.cpkl'
 cPickle.dump(frbx['Emission_OVI'],open(fileout,'wb'),protocol=-1)
-fileout = 'frby_250pc_OVI.cpkl'
+fileout = 'frby_5kpc_OVI.cpkl'
 cPickle.dump(frby['Emission_OVI'],open(fileout,'wb'),protocol=-1)
-fileout = 'frbz_250pc_OVI.cpkl'
+fileout = 'frbz_5kpc_OVI.cpkl'
 cPickle.dump(frbz['Emission_OVI'],open(fileout,'wb'),protocol=-1)
 
+fileout = 'frbx_5kpc_OVI_Scaled.cpkl'
+cPickle.dump(frbx['Emission_OVI_Scaled'],open(fileout,'wb'),protocol=-1)
+fileout = 'frby_5kpc_OVI_Scaled.cpkl'
+cPickle.dump(frby['Emission_OVI_Scaled'],open(fileout,'wb'),protocol=-1)
+fileout = 'frbz_5kpc_OVI_Scaled.cpkl'
+cPickle.dump(frbz['Emission_OVI_Scaled'],open(fileout,'wb'),protocol=-1)
 
-
+fileout = 'frbx_5kpc_OVI_Scaled_ncut.cpkl'
+cPickle.dump(frbx['Emission_OVI_Scaled_ncut'],open(fileout,'wb'),protocol=-1)
+fileout = 'frby_5kpc_OVI_Scaled_ncut.cpkl'
+cPickle.dump(frby['Emission_OVI_Scaled_ncut'],open(fileout,'wb'),protocol=-1)
+fileout = 'frbz_5kpc_OVI_Scaled_ncut.cpkl'
+cPickle.dump(frbz['Emission_OVI_Scaled_ncut'],open(fileout,'wb'),protocol=-1)
 
 
 
