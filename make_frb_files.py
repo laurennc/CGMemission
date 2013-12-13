3664e5e756c6bf4671651d1052dbb8a591a436c0
laurennc <lcorlie@gmail.com>
2013-12-13 10:25:33 -0500

    create CIII files

--- make_frb_files.py
+++ make_frb_files.py
@@ -6,8 +6,9 @@ pf = load(fn, file_style="%s.grid.cpu%%04i") # load data
 
 #fields = ["HAlpha_Emissivity_R","Emission_CIV","Emission_OVI"]
 #fields = ["Emission_CIV","Emission_OVI","Emission_CIV_Scaled","Emission_OVI_Scaled","Emission_CIV_Scaled_ncut","Emission_OVI_Scaled_ncut"]
+#fields = ["Emission_CIII_977","Emission_CIII_977_Scaled","Emission_CIII_977_Scaled_ncut"]
+fields = ["Emission_CIII","Emission_CIII_Scaled","Emission_CIII_Scaled_ncut"]
 
-fields = ["Emission_CIII_977","Emission_CIII_977_Scaled","Emission_CIII_977_Scaled_ncut"]
 
 width = 200./pf['kpc']
 #res = [800,800]
@@ -30,12 +31,18 @@ frbx = projx.to_frb(width,res,center=pos)
 #cPickle.dump(frbx['Emission_OVI_Scaled'],open(fileout,'wb'),protocol=-1)
 #fileout = 'frbx_5kpc_OVI_Scaled_ncut.cpkl'
 #cPickle.dump(frbx['Emission_OVI_Scaled_ncut'],open(fileout,'wb'),protocol=-1)
-fileout = 'frbx_5kpc_CIII_977.cpkl'
-cPickle.dump(frbx['Emission_CIII_977'],open(fileout,'wb'),protocol=-1)
-fileout = 'frbx_5kpc_CIII_977_Scaled.cpkl'
-cPickle.dump(frbx['Emission_CIII_977_Scaled'],open(fileout,'wb'),protocol=-1)
-fileout = 'frbx_5kpc_CIII_977_Scaled_ncut.cpkl'
-cPickle.dump(frbx['Emission_CIII_977_Scaled_ncut'],open(fileout,'wb'),protocol=-1)
+fileout = 'frbx_5kpc_CIII.cpkl'
+cPickle.dump(frbx['Emission_CIII'],open(fileout,'wb'),protocol=-1)
+fileout = 'frbx_5kpc_CIII_Scaled.cpkl'
+cPickle.dump(frbx['Emission_CIII_Scaled'],open(fileout,'wb'),protocol=-1)
+fileout = 'frbx_5kpc_CIII_Scaled_ncut.cpkl'
+cPickle.dump(frbx['Emission_CIII_Scaled_ncut'],open(fileout,'wb'),protocol=-1)
+#fileout = 'frbx_5kpc_CIII_977.cpkl'
+#cPickle.dump(frbx['Emission_CIII_977'],open(fileout,'wb'),protocol=-1)
+#fileout = 'frbx_5kpc_CIII_977_Scaled.cpkl'
+#cPickle.dump(frbx['Emission_CIII_977_Scaled'],open(fileout,'wb'),protocol=-1)
+#fileout = 'frbx_5kpc_CIII_977_Scaled_ncut.cpkl'
+#cPickle.dump(frbx['Emission_CIII_977_Scaled_ncut'],open(fileout,'wb'),protocol=-1)
 
 projy = pf.h.proj(1,fields)
 frby = projy.to_frb(width,res,center=pos)
@@ -52,13 +59,18 @@ frby = projy.to_frb(width,res,center=pos)
 #cPickle.dump(frby['Emission_OVI_Scaled'],open(fileout,'wb'),protocol=-1)
 #fileout = 'frby_5kpc_OVI_Scaled_ncut.cpkl'
 #cPickle.dump(frby['Emission_OVI_Scaled_ncut'],open(fileout,'wb'),protocol=-1)
-fileout = 'frby_5kpc_CIII_977.cpkl'
-cPickle.dump(frby['Emission_CIII_977'],open(fileout,'wb'),protocol=-1)
-fileout = 'frby_5kpc_CIII_977_Scaled.cpkl'
-cPickle.dump(frby['Emission_CIII_977_Scaled'],open(fileout,'wb'),protocol=-1)
-fileout = 'frby_5kpc_CIII_977_Scaled_ncut.cpkl'
-cPickle.dump(frby['Emission_CIII_977_Scaled_ncut'],open(fileout,'wb'),protocol=-1)
-
+#fileout = 'frby_5kpc_CIII_977.cpkl'
+#cPickle.dump(frby['Emission_CIII_977'],open(fileout,'wb'),protocol=-1)
+#fileout = 'frby_5kpc_CIII_977_Scaled.cpkl'
+#cPickle.dump(frby['Emission_CIII_977_Scaled'],open(fileout,'wb'),protocol=-1)
+#fileout = 'frby_5kpc_CIII_977_Scaled_ncut.cpkl'
+#cPickle.dump(frby['Emission_CIII_977_Scaled_ncut'],open(fileout,'wb'),protocol=-1)
+fileout = 'frby_5kpc_CIII.cpkl'
+cPickle.dump(frby['Emission_CIII'],open(fileout,'wb'),protocol=-1)
+fileout = 'frby_5kpc_CIII_Scaled.cpkl'
+cPickle.dump(frby['Emission_CIII_Scaled'],open(fileout,'wb'),protocol=-1)
+fileout = 'frby_5kpc_CIII_Scaled_ncut.cpkl'
+cPickle.dump(frby['Emission_CIII_Scaled_ncut'],open(fileout,'wb'),protocol=-1)
 
 projz = pf.h.proj(2,fields)
 frbz = projz.to_frb(width,res,center=pos)
@@ -75,13 +87,18 @@ frbz = projz.to_frb(width,res,center=pos)
 #cPickle.dump(frbz['Emission_OVI_Scaled'],open(fileout,'wb'),protocol=-1)
 #fileout = 'frbz_5kpc_OVI_Scaled_ncut.cpkl'
 #cPickle.dump(frbz['Emission_OVI_Scaled_ncut'],open(fileout,'wb'),protocol=-1)
-fileout = 'frbz_5kpc_CIII_977.cpkl'
-cPickle.dump(frbz['Emission_CIII_977'],open(fileout,'wb'),protocol=-1)
-fileout = 'frbz_5kpc_CIII_977_Scaled.cpkl'
-cPickle.dump(frbz['Emission_CIII_977_Scaled'],open(fileout,'wb'),protocol=-1)
-fileout = 'frbz_5kpc_CIII_977_Scaled_ncut.cpkl'
-cPickle.dump(frbz['Emission_CIII_977_Scaled_ncut'],open(fileout,'wb'),protocol=-1)
-
+#fileout = 'frbz_5kpc_CIII_977.cpkl'
+#cPickle.dump(frbz['Emission_CIII_977'],open(fileout,'wb'),protocol=-1)
+#fileout = 'frbz_5kpc_CIII_977_Scaled.cpkl'
+#cPickle.dump(frbz['Emission_CIII_977_Scaled'],open(fileout,'wb'),protocol=-1)
+#fileout = 'frbz_5kpc_CIII_977_Scaled_ncut.cpkl'
+#cPickle.dump(frbz['Emission_CIII_977_Scaled_ncut'],open(fileout,'wb'),protocol=-1)
+fileout = 'frbz_5kpc_CIII.cpkl'
+cPickle.dump(frbz['Emission_CIII'],open(fileout,'wb'),protocol=-1)
+fileout = 'frbz_5kpc_CIII_Scaled.cpkl'
+cPickle.dump(frbz['Emission_CIII_Scaled'],open(fileout,'wb'),protocol=-1)
+fileout = 'frbz_5kpc_CIII_Scaled_ncut.cpkl'
+cPickle.dump(frbz['Emission_CIII_Scaled_ncut'],open(fileout,'wb'),protocol=-1)
 
 
 #patt = 'frbs/frb%c_250pc_Ha.cpkl'
