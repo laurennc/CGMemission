70105b029d53919344b82e5264fe43e3bae9495d
laurennc <lcorlie@gmail.com>
2014-05-20 15:27:36 -0400

    does what it says

---
+++ basic_slice.py
@@ -0,0 +1,28 @@
+from lauren import *
+import matplotlib
+
+fn="/u/10/l/lnc2115/vega/data/Ryan/r0058_l10/redshift0058"
+
+pf = load(fn, file_style="%s.grid.cpu%%04i") # load data
+
+pos = [0.40328598,0.47176743,0.46131516]
+rad = 108.0/pf['kpc']
+
+data_source = pf.h.sphere(pos,rad)
+
+field = ['Density','HI_NumberDensity']
+
+font = {'weight':'bold','size':22,'family':'sans-serif'}
+matplotlib.rc('font',**font)
+
+sp = SlicePlot(pf,'y',field,center=pos,width=(108.,'kpc'),axes_unit=['kpc','kpc'],fontsize=22.)
+
+sp.annotate_particles(0.05,p_size=1.0,dm_only=True,alpha=0.03) #also stars_only keyword
+
+sp.set_font(font)
+sp.set_cmap('all','spectral')
+#pp.set_zlime('all',low,high)
+
+sp.save('003')
+
+
