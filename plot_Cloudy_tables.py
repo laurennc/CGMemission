b68665d9dd1c34c8ea009a38c997e288b09f076e
laurennc <lcorlie@gmail.com>
2014-05-29 14:31:45 -0400

    looking at different cloudy models

--- plot_Cloudy_tables.py
+++ plot_Cloudy_tables.py
@@ -1,14 +1,16 @@
 from lauren import *
 from plotting_routines import *
 
-#patt = "/u/10/l/lnc2115/vega/data/Ryan/cloudy_out/bertone_exact/bertone_exact_run"
+patt = "/u/10/l/lnc2115/vega/data/Ryan/cloudy_out/bertone_exact/z000/bertone_exact_run"
 
-patt = "/u/10/l/lnc2115/vega/data/Ryan/cloudy_out/all_lines/all_emissivity_run"
+#patt = "/u/10/l/lnc2115/vega/data/Ryan/cloudy_out/all_lines/all_emissivity_run"
+#patt =  "/u/10/l/lnc2115/vega/data/Ryan/cloudy_out/euvb/euvb_run"
 
 
 #files with hden = 1, -3, -6 respectively
-inputfiles = [patt+'15.dat',patt+'7.dat',patt+'1.dat']
-outputfile = 'coronal_models.png'
+#inputfiles = [patt+'15.dat',patt+'7.dat',patt+'1.dat']
+inputfiles = [patt+'3.dat',patt+'2.dat',patt+'1.dat']
+outputfile = 'bertone_exact_double_models.png'
 xlen = 3
 ylen = 1
 
