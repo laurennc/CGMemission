0c2d0144157e6d711babb9e1cdf561102c215285
laurennc <lcorlie@gmail.com>
2014-06-10 16:26:23 -0400

    changed to work with the new column densities frbs that ive made

--- SB_radial_plots.py
+++ SB_radial_plots.py
@@ -1,6 +1,6 @@
 import matplotlib as mpl
 mpl.use('agg')
-from lauren import *
+from lauren import make_SB_profile
 from radial_data_lauren import *
 import matplotlib.pyplot as plt
 import cPickle
@@ -23,9 +23,9 @@ def make_plots():
 	#F = pylab.gcf()
 	#F.set_size_inches(8.75,5.5)
 
-	patt = "/u/10/l/lnc2115/vega/repos/CGMemission/ColDensFRBs/frb"
+	patt = "/u/10/l/lnc2115/vega/repos/CGMemission/euvb_frbs/coldens/frb"
 
-	pattends = ["_250pc_CIII_coldens.cpkl","_250pc_CIV_coldens.cpkl","_250pc_OVI_coldens.cpkl","_250pc_MgII_coldens.cpkl","_250pc_SiII_coldens.cpkl","_250pc_SiIII_coldens.cpkl","_250pc_SiIV_coldens.cpkl","_250pc_HI_coldens.cpkl"]	
+	pattends = ["_250pc_CIIIdens.cpkl","_250pc_CIVdens.cpkl","_250pc_OVIdens.cpkl","_250pc_MgIIdens.cpkl","_250pc_SiIIdens.cpkl","_250pc_SiIIIdens.cpkl","_250pc_SiIVdens.cpkl","_250pc_HIdens.cpkl"]	
 
 	ions = ['C III','C IV','O VI','Mg II',]
 
@@ -39,19 +39,19 @@ def make_plots():
 		rpr,rpmean,rpmedian,rpmax,rpmin,rpstd = make_SB_profile(patt+"x"+pattend,patt+"y"+pattend,patt+"z"+pattend)
 		ax1 = fig.add_subplot(iax)
 		ax1.plot(rpr,np.log10(rpmedian),'k-',linewidth=1.2,label=re.split('c_|_c',pattend)[1])
-		key = re.split('c_|_c',pattend)[1]
-		ax1.fill_between(rpr,np.log10(rpmedian-rpstd),np.log10(rpmedian+rpstd),color='gray',alpha='0.30')
+		key = re.split('c_|dens',pattend)[1]
+		ax1.fill_between(rpr,np.log10(rpmin),np.log10(rpmax),color='gray',alpha='0.30')
 		plot_Werk_ColDens(ax1,data,key,'Rperp')
 		ax1.set_xscale('log')
 		#ax1.set_ylim(12,20)
 		ax1.set_xlim(8,200)
-		ax1.text(12,15.05,re.split('c_|_c',pattend)[1])
+		ax1.text(12,15.05,re.split('c_|dens',pattend)[1])
 		#ax1.set_xlabel('Radius (kpc)')
 		#ax1.set_ylabel('Column Density [cm^-2]')	
 		iax = iax + 1
 		
 	plt.gcf().subplots_adjust(bottom=0.15)
-	plt.savefig('coldens_profs_werk_std.png')
+	plt.savefig('euvb_coldens_profs_werk.png')
 	plt.close()
 
 
