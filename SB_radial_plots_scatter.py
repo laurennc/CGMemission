9f256481bba7352f98e303aba57678b4cfe52d3a
laurennc <lcorlie@gmail.com>
2014-09-10 14:01:12 -0400

    plot changed so the points have an opacity that varies with radius

--- SB_radial_plots_scatter.py
+++ SB_radial_plots_scatter.py
@@ -19,13 +19,12 @@ matplotlib.rc('font',**font)
 #res = [200,200]
 #res = [40,40]
 #xL2 = np.arange(-100,100,0.25)
-res = []
 xL2 = np.linspace(-160,160,320)
 
 
-patt = "/u/10/l/lnc2115/vega/repos/CGMemission/bertone_frbs/emis/control/frb"
+patt = "/u/10/l/lnc2115/vega/repos/CGMemission/bertone_frbs/emis/grid_galquas/g1q01/frb"
 #patt =  "/u/10/l/lnc2115/vega/repos/CGMemission/euvb_frbs/emis/frb"
-pattends = ["_250pc_CIII.cpkl","_250pc_CIII_977.cpkl","_250pc_CIV.cpkl","_250pc_OVI.cpkl","_250pc_MgII.cpkl","_250pc_SiII.cpkl","_250pc_SiIII_1207.cpkl","_250pc_SiIV.cpkl","_250pc_HAlpha.cpkl"]#,"_250pc_SiIII_1883.cpkl","_250pc_SiIV.cpkl","_250pc_HAlpha.cpkl"]
+pattends = ["_1kpc_z02_CIII.cpkl","_1kpc_z02_CIII_977.cpkl","_1kpc_z02_CIV.cpkl","_1kpc_z02_OVI.cpkl","_1kpc_z02_MgII.cpkl","_1kpc_z02_SiII.cpkl","_1kpc_z02_SiIII_1207.cpkl","_1kpc_z02_SiIV.cpkl","_1kpc_z02_HAlpha.cpkl"]#,"_1kpc_z02_SiIII_1883.cpkl","_1kpc_z02_SiIV.cpkl","_1kpc_z02_HAlpha.cpkl"]
 
 #iax = 221
 iax = 331
@@ -52,24 +51,24 @@ for i in range(len(pattends)):
 
 	rpr,rpmean,rpmedian,rpmax,rpmin,rpstd = make_SB_profile(patt+"x"+pattend,patt+"y"+pattend,patt+"z"+pattend)
 
-	idr = np.where(rpr <= 100.0)[0]
-	ax1.plot(rpr[idr],np.log10(rpmedian[idr]),'k',linewidth=2.2,label=re.split('c_|.cp',pattend)[1])
+	idr = np.where(rpr <= 160.0)
 
-	plot_scatter_percentile(ax1,cPickle.load(open(patt+"x"+pattend,'rb')),xL2,xL2,0.01,'k.',maxr=100.)
+	plot_scatter_percentile(ax1,cPickle.load(open(patt+"x"+pattend,'rb')),xL2,xL2,0.05,'k.',maxr=160.)
+	ax1.plot(rpr[idr],np.log10(rpmedian[idr]),color='Goldenrod',linewidth=2.2,label=re.split('z02_|.cp',pattend)[1])
 
 	#ax1.set_xscale('log')
         #ax1.set_xlabel('Radius (kpc)')
         #ax1.set_ylabel('log(Surface Brightness) (LU)')
         #ax1.axis([8.,200.,-4,7])
         ax1.legend(loc='upper right',prop={'size':12})
-	ax1.axis([0.,100.,-8,8])
+	ax1.axis([0.,160.,-8,8])
 	iax = iax + 1
 	i = i + 1
 
 plt.gcf().subplots_adjust(bottom=0.15)
 #plt.savefig('euvb_frbs/euvb_scatter_profile.png')
 #plt.savefig('euvb_frbs/euvb_scatter_profile2.png')
-plt.savefig('bertone_frbs/bertone_control_scatter_limits.png')
+plt.savefig('bertone_frbs/g1q01_scatter_limits.png')
 plt.close()
 
 
