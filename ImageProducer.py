7aa0a653963ad6c0c312d5538bb4c3f515186481
laurennc <lcorlie@gmail.com>
2015-03-30 12:01:34 -0400

    updated so that the wavelengths generator is more flexible. will also write fits files correctly

--- ImageProducer.py
+++ ImageProducer.py
@@ -4,6 +4,7 @@ import cPickle
 from astropy.convolution import convolve, convolve_fft
 from astropy.convolution import Gaussian1DKernel
 from astropy.io import fits
+import os 
 
 class ImageProducer(object):
 	"""ImageProducer receives one of the frb image arrays that I've produced.
@@ -11,50 +12,67 @@ class ImageProducer(object):
 	   to build the wavelength dimension. It will also scale the image for the 
 	   correct redshift of interest.
 
-	   The wavelengts are given in microns as is required by the IMO. The output
-	   is in ergs cm^-2 s^-1 sr^-1 A^-1 """
+	   The output has units of photons cm^-2 s^-1 sr^-1 A^-1 """
 
-	def __init__(self, frbarray,lambda_center,lambda_range,lambda_res,step_width,z_image,z_observed):
-		#self.frb = cPickle.load(open(frbarray,'rb'))
+	def __init__(self, frbarray,lambda_center,lambda_range,lambda_res,lambda_line,step_width,z_observed):
+	#"""The inputs are as follows:
+	#   frbarray = output projection of the simulation
+        #   lambda_center = central wavelength of the desired wavelength window
+        #   lambda_range = +/- this value covers the full wavelength range desired
+        #   lambda_res = resolution of spectral dimension
+        #   lambda_line = wavelength of the line being observed
+        #   step_width_z = how wide should the step function be that approximates the simulation signal in resolution units
+        #   z_observed = the redshift of the observation for scaling the flux
+#
+        #   The output variables are as follows:
+        #   img = array that will contain the convolved imaged. Initialized as 0  """
+
 		self.frb = frbarray
-		#The wavelengths are set for the initial conditions agreed upon for the IMO
 		self.wavelengths = np.arange(lambda_center-lambda_range,lambda_center+lambda_range+lambda_res,lambda_res)[:-1]
 		#self.wavelengths = np.arange(0.204-0.0012,0.204+0.0012+3.4e-6,3.4e-6)
 		
 		#build the step function that will lead to the convolver shape
 		self.step_function = np.zeros(len(self.wavelengths))
-		center = (len(self.wavelengths)-1.)/2.
-		##are these the right units??
-		self.step_function[center-step_width/2.:center+step_width/2.+1] = 1.0/(step_width*lambda_res)
+		idx = (np.abs(self.wavelengths-lambda_line)).argmin()
+		self.step_function[idx-step_width/2.:idx+step_width/2.+1] = 1.0/(step_width*lambda_res)
 
-		self.z_image = z_image
 		self.z_observed = z_observed
 
 		self.img = 0
 
-		##CAN DECIDE LATER IF I'D LIKE IT TO GET THE CENTRAL WAVELENGTH FROM THE FILE NAME
-		##OR IF IT'S AN INPUT I WOULD LIKE TO OFFER
-
 	def convolve_image(self,stddev,scale=True):
-		##CURRENTLY BUILDING IMAGE AS LAMBA,X,Y
-		img = np.zeros((len(self.wavelengths),len(self.frb[0,:]),len(self.frb[1,:])))
+       # """The inputs are as follows:
+       #    stddev = standard deviation of the Gaussian the step function will be convolved with
+       #    scale = True -- will scale the flux by the redshift unless set to False
+#
+       #    The output variables are as follows:
+       #    Nothing is returned but the object property, img, is updated to hold the convolved image
+       #  """		
+
+##CURRENTLY BUILDING IMAGE AS LAMBA,X,Y
+		##Because writing the fits file transposes the x and z axes for some reason
+		self.img = np.zeros((len(self.wavelengths),len(self.frb[0,:]),len(self.frb[1,:])))
 
 
-		gauss = Gaussian1DKernel(stddev=stddev)
+		gauss = Gaussian1DKernel(stddev=stddev,x_size=len(self.wavelengths))
 		zconvolve = convolve(self.step_function,gauss.array,boundary='extend')
-
 		
 		for i in range(len(self.frb[0,:])):
 			for j in range(len(self.frb[1,:])):
 				if scale:
-					img[:,i,j] = zconvolve*(self.frb[:,i,j]/((1.+self.z_observed)**4.0))
+					self.img[:,i,j] = zconvolve*(self.frb[i,j]/((1.+self.z_observed)**4.0))
 				else:
-					img[:,i,j] = zconvolve*self.frb[:,i,j]
+					self.img[:,i,j] = zconvolve*self.frb[i,j]
 
-		return img
+		return
 
 	def save_as_fits(self,outputname):
-		hdu = fits.PrimaryHDU(img)
+	#Save the convolution as a fits file
+		try:
+			os.remove(outputname)
+		except OSError:
+			pass
+		hdu = fits.PrimaryHDU(self.img)
 		hdu.writeto(outputname)		
 		return
 
