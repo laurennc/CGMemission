693e7456bf9bcc5e40c45a59e3e21a801f0c3839
laurennc <lcorlie@gmail.com>
2015-04-12 08:50:04 -0400

    I added code to convert values entre LU and ergs

--- lauren.py
+++ lauren.py
@@ -10,6 +10,16 @@ import triangle
 def ergs_sr_TO_raleighs(data_arr):
 	return data_arr*3.304e11/79577.4715459
 
+def LU_to_ergs(val,l_emit):
+	#LU to ergs s^-1 cm^-2 arcsec^-2	
+	#l_emit MUST BE IN ANGSTROMS
+	return (val*4.6690e-19)/(l_emit)
+
+def ergs_to_LU(val,l_emit):
+	#ergs to LU = photons s^-1 sr^-1 cm^-2
+	#l_emit MUST BE IN ANGSTROMS
+	return (l_emit*val)*(2.1418e18)
+
 def distance_from_center(x,y,z,center):
         return ((x-center[0])**2.0+(y-center[1])**2.0+(z-center[2])**2.0)**0.5
 
@@ -30,52 +40,18 @@ def scale_by_energy(frbfile,energy):
 	data = cPickle.load(open(frbfile,'rb'))
         return data*(5.7e-18)*(1./1.87e-12)/(4.*np.pi*energy)
 
-def plot_scatter_percentile(ax1,data,x,y,percentile,symbol,maxr=500.):
-	#data is the frb array data
-	x,y = np.meshgrid(x,y)
-	working_mask = np.ones(data.shape,bool)
-	r = abs(x+1j*y)
-	rmax = r[working_mask].max()
-        dr = np.abs([x[0,0] - x[0,1]]) #* annulus_width
-        radial = np.arange(maxr/dr)*dr + dr/2.
-        nrad = len(radial)	
-	#mslope = (0.001-0.007)/nrad
-	mslope = (0.0075-0.07)/nrad
-
-	for irad in range(nrad):
-		minrad = irad*dr
-		maxrad = minrad + dr
-		thisindex = (r>=minrad) * (r<maxrad) * working_mask
-		alphahere = mslope*irad + 0.07
-		ax1.plot(r[thisindex],np.log10(data[thisindex]),symbol,alpha=alphahere)
- 		rhere = r[thisindex].flatten()
-		datahere = data[thisindex].flatten()
-		meanhere = data[thisindex].mean()
-		lenperc = int(len(datahere)*percentile)
-		idSort = np.argsort(datahere)[::-1]
-		wanted = idSort[0:lenperc]
-		if len(wanted) == 0:
-			wanted = np.where(datahere == datahere.max())
-		ax1. plot(rhere[wanted],np.log10(datahere[wanted]),'g.',alpha=0.2)
-		#ax1.plot(rhere[wanted],np.log10(datahere[wanted]),symbol,markersize=3.5)#,markersize=0.75)				
-	return
-
 def make_Cloudy_table(table_index):
 	#hden_n_bins, hden_min, hden_max = 35, -6, 1
 	#T_n_bins, T_min, T_max = 151, 3, 8
 	hden_n_bins, hden_min, hden_max = 15, -6, 1
 	#hden_n_bins, hden_min, hden_max = 17, -6, 2
 	T_n_bins, T_min, T_max = 51, 3, 8
-	#patt = "/u/10/l/lnc2115/vega/data/Ryan/cloudy_out/euvb/euvb_run%i.dat"
-	#patt = "/u/10/l/lnc2115/vega/data/Ryan/cloudy_out/bertone/bertone_run%i.dat"
-	#patt = "/u/10/l/lnc2115/vega/data/Ryan/cloudy_out/bertone_factor1/bertone1_run%i.dat"
-	patt = "/u/10/l/lnc2115/vega/data/Ryan/cloudy_out/grid_galquas/emis/z05/g1q1/g1q1_run%i.dat"
+	patt = "/u/10/l/lnc2115/vega/data/Ryan/cloudy_out/grid_galquas/emis/z05/g1q10/g1q10_run%i.dat"
 
 	hden=numpy.linspace(hden_min,hden_max,hden_n_bins)
 	T=numpy.linspace(T_min,T_max, T_n_bins)
 	table = np.zeros((hden_n_bins,T_n_bins))
 	for i in range(hden_n_bins):
-		#print i
 		table[i,:]=[float(l.split()[table_index]) for l in open(patt%(i+1)) if l[0] != "#"]
 	return hden,T,table
 
@@ -83,9 +59,7 @@ def make_ion_table(ion,number):
 	#hden_n_bins,hden_min,hden_max = 15, -6, 1
 	hden_n_bins, hden_min, hden_max = 17, -6, 2
 	T_n_bins, T_min, T_max = 51, 3, 8
-	#patt = "/u/10/l/lnc2115/vega/data/Ryan/cloudy_out/Ions/control/euvb/euvb_ion_run%i_"+ion+".dat"
-	#patt = "/u/10/l/lnc2115/vega/data/Ryan/cloudy_out/Ions/euvbIon_factor1000/euvb_ion_run%i_"+ion+".dat"
-	patt = "/u/10/l/lnc2115/vega/data/Ryan/cloudy_out/Ions/grid_galquas/g1q10/bert_ion_run%i_"+ion+".dat"
+	patt = "/u/10/l/lnc2115/vega/data/Ryan/cloudy_out/Ions/grid_galquas/g1q01/bert_ion_run%i_"+ion+".dat"
 	hden=numpy.linspace(hden_min,hden_max,hden_n_bins)
         T=numpy.linspace(T_min,T_max, T_n_bins)
         table = np.zeros((hden_n_bins,T_n_bins))
@@ -101,11 +75,8 @@ def make_SB_profile(filex,filey,filez,xL,z=0.):
 	xL, yL = np.meshgrid(xL,xL)
         #r = abs(xL+1j*yL)
 
-#	print filex
         frbx = cPickle.load(open(filex,'rb'))
- #       print filey
 	frby = cPickle.load(open(filey,'rb'))
-  #      print filez
 	frbz = cPickle.load(open(filez,'rb'))
 
         rp_x = radial_data(frbx/(1.+z)**4.0,x=xL,y=yL)
@@ -176,4 +147,35 @@ def emission_interpolation(field_idx,H_N,Temperature):
 	idx = np.isnan(dia)
 	dia[idx] = -200.0
 	return dia
+
+def plot_scatter_percentile(ax1,data,x,y,percentile,symbol,maxr=500.):
+        #data is the frb array data
+        x,y = np.meshgrid(x,y)
+        working_mask = np.ones(data.shape,bool)
+        r = abs(x+1j*y)
+        rmax = r[working_mask].max()
+        dr = np.abs([x[0,0] - x[0,1]]) #* annulus_width
+        radial = np.arange(maxr/dr)*dr + dr/2.
+        nrad = len(radial)
+        #mslope = (0.001-0.007)/nrad
+        mslope = (0.0075-0.07)/nrad
+
+        for irad in range(nrad):
+                minrad = irad*dr
+                maxrad = minrad + dr
+                thisindex = (r>=minrad) * (r<maxrad) * working_mask
+                alphahere = mslope*irad + 0.07
+                ax1.plot(r[thisindex],np.log10(data[thisindex]),symbol,alpha=alphahere)
+                rhere = r[thisindex].flatten()
+                datahere = data[thisindex].flatten()
+                meanhere = data[thisindex].mean()
+                lenperc = int(len(datahere)*percentile)
+                idSort = np.argsort(datahere)[::-1]
+                wanted = idSort[0:lenperc]
+                if len(wanted) == 0:
+                        wanted = np.where(datahere == datahere.max())
+                ax1. plot(rhere[wanted],np.log10(datahere[wanted]),'g.',alpha=0.2)
+                #ax1.plot(rhere[wanted],np.log10(datahere[wanted]),symbol,markersize=3.5)#,markersize=0.75)                             
+        return
+
 
