7956877343c15586d166ac17ccdd4eb8bc90cd8f
laurennc <lcorlie@gmail.com>
2015-06-26 07:04:01 -0400

    unsure...

--- finding_virial_mass.py
+++ finding_virial_mass.py
@@ -1,12 +1,23 @@
 from yt.mods import *
 import numpy as np
 import cPickle
+import yt.analysis_modules.halo_profiler.api as HP
 
 fn="/u/10/l/lnc2115/vega/data/Ryan/r0054/redshift0054"
 pf = load(fn, file_style="%s.grid.cpu%%04i")
 
-
+#def write_halo_list(pf):
 halo_list = HaloFinder(pf)
-halo_list.write_out("HopAnaylsis.out")
+halo_list.write_out("HopAnaylsis_z02.out")
+#	return
 
+#def finding_halo_properties(fn):
+#hp = HP.HaloProfiler(fn, halo_list_file='HopAnalysis.out')
+#hp.add_halo_filter(HP.VirialFilter,must_be_virialized=True,
+#              overdensity_field='ActualOverdensity',
+#              virial_overdensity=200,
+#              virial_filters=[['TotalMassMsun','>=','1e8']],
+#              virial_quantities=['TotalMassMsun','RadiusMpc'])
+#hp.make_profiles(filename="VirialHaloes.out")
+	
 
