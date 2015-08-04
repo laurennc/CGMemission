0fd89569dbf30a383b8a9086a9b90b660ff23f2e
laurennc <lcorlie@gmail.com>
2015-08-04 12:48:59 -0400

    trying to calculate virial quantities

--- finding_virial_mass.py
+++ finding_virial_mass.py
@@ -1,14 +1,25 @@
+import matplotlib
+matplotlib.use('Agg')
 from yt.mods import *
 import numpy as np
 import cPickle
 import yt.analysis_modules.halo_profiler.api as HP
 
-fn="/u/10/l/lnc2115/vega/data/Ryan/r0054/redshift0054"
+##################### z = 0 #########################
+#fn="/u/10/l/lnc2115/vega/data/Ryan/r0058_l10/redshift0058"
+#pos = [0.40328598,0.47176743,0.46131516]
+##################### z = 0.2 ##########################
+#fn="/u/10/l/lnc2115/vega/data/Ryan/r0054/redshift0054"
+####################  z = 0.5 ###########################
+#fn="/u/10/l/lnc2115/vega/data/Ryan/r0048/redshift0048"
+####################  z = 1.0 ##########################
+fn="/u/10/l/lnc2115/vega/data/Ryan/r0038/redshift0038"
+
 pf = load(fn, file_style="%s.grid.cpu%%04i")
 
 #def write_halo_list(pf):
 halo_list = HaloFinder(pf)
-halo_list.write_out("HopAnaylsis_z02.out")
+halo_list.write_out("HopAnaylsis_z1.out")
 #	return
 
 #def finding_halo_properties(fn):
