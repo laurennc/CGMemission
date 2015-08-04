b9c73ec729328bc96c1746ff000f885531866408
laurennc <lcorlie@gmail.com>
2015-08-04 12:49:42 -0400

    phase plots for the paper

--- plots_for_paper.py
+++ plots_for_paper.py
@@ -4,30 +4,33 @@ import matplotlib.pyplot as plt
 from yt.mods import *
 import numpy as np
 import sys
+import matplotlib.colorbar as cb
 
 fn="/u/10/l/lnc2115/vega/data/Ryan/r0058_l10/redshift0058"
+orient = 'horizontal'
 
 pf = load(fn, file_style="%s.grid.cpu%%04i") # load data
-
 val, pos = pf.h.find_max('Density')
-#pos = [0.40328598,0.47176743,0.46131516]
-#rad = 108.0/pf['kpc']
-#rad = 320./pf['kpc']
+
+fig,axes,colorbars = get_multi_plot(3,1,colorbar=orient,bw=4)
 
 pc = PlotCollection(pf,pos)
-p = pc.add_phase_sphere(1000.,'kpc',["Radiuskpc","Temperature","CellMassMsun"],weight=None)
-pc.set_cmap('spectral')
-pc.save('paper1')
 
-pc = PlotCollection(pf,pos)
-p = pc.add_phase_sphere(1000.,'kpc',["Radiuskpc","Metallicity","CellMassMsun"],weight=None,cmap='spectral',x_log=True,x_bounds=(1.,1000.),y_log=True,y_bounds=(-5,10),axes=[])
-pc.set_cmap('spectral')
-pc.save('paper1')
+p = pc.add_phase_sphere(1000.,'kpc',["Radiuskpc","Temperature","CellMassMsun"],weight=None,x_log=True,x_bounds=(1.,1000.),y_log=True,y_bounds=(10**4.,10**7.),figure=fig,axes=[0,1])
+p.set_cmap('spectral')
 
-pc = PlotCollection(pf,pos)
-p = pc.add_phase_sphere(1000.,'kpc',["Radiuskpc","Density","CellMassMsun"],weight=None)
-pc.set_cmap('gnuplot2')
-pc.save('paper1')
+p = pc.add_phase_sphere(1000.,'kpc',["Radiuskpc","Metallicity","CellMassMsun"],weight=None,x_log=True,x_bounds=(1.,1000.),y_log=True,y_bounds=(10**-5.,10),figure=fig,axes=[0,2])
+p.set_cmap('spectral')
+
+p = pc.add_phase_sphere(1000.,'kpc',["Radiuskpc","Density","CellMassMsun"],weight=None,x_log=True,x_bounds=(1.,1000.),y_log=True,y_bounds=(10**-32.,10**-22.),figure=fig,axes=[0,0])
+p.set_cmap('gnuplot2')
+
+for p,cax in zip(pc.plots,colorbars):
+	cbar = cb.Colorbar(cax,p.image,orientation=orient)
+	p.colorbar = cbar
+	p._autoset_label()
+
+fig.savefig('paper1_sim_basic_profiles.png')
 
 
 
