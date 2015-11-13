82cc1d53c97414d7bd6719ba5467aa4b5779ed30
laurennc <lcorlie@gmail.com>
2015-11-13 01:40:51 -0500

    updated some things like filenames, axis labels etc

--- frb_covering_fractions.py
+++ frb_covering_fractions.py
@@ -28,16 +28,17 @@ obs_colors = True
 
 ##REDSHIFT EVOLUTION PARAMETERS
 model_beg = '/u/10/l/lnc2115/vega/repos/CGMemission/bertone_frbs/final/emis/'
-temperature_beg = '/u/10/l/lnc2115/vega/repos/CGMemission/bertone_frbs/final/basic/'
+temp_beg = '/u/10/l/lnc2115/vega/repos/CGMemission/bertone_frbs/final/basic/'
 model_gqs = ['g1q1','g1q1','g1q1','g1q1']
 res_keys = ['1kpc','1kpc','1kpc','1kpc']
 redshift_keys = ['z0','z02','z05','z1']
 znow = [0.,0.2,0.5,1.0]
 #znow = [0.,0.,0.,0.]
+zplot = [0.,0.2,0.5,1.0]
 ions = ['SiIV','CIII_977','CIV','OVI']	
 xlen,ylen = 3,1
 figxlen,figylen = 12,4
-fileout = 'frb_covering_fraction.png'#_nozscaling.png'
+fileout = 'frb_covering_fraction_nodisk.png'#_nozscaling.png'
 colors = ['Red','Magenta','Green','Blue']
 SB_lims = [1.,2.,3.]
 
@@ -52,17 +53,17 @@ for ion in ions:
 	lim1 = []
 	lim2 = []
 	lim3 = []
-	zplot = []
 	while count < len(model_gqs):
 		print ion, count, model_gqs[count],i
 		modelnames = [model_beg+redshift_keys[count]+'/'+model_gqs[count]+'/frbx_'+res_keys[count]+'_500kpc_'+redshift_keys[count]+'_'+ion+'.cpkl'] 
-		tempname = model_beg+redshift_keys[count]+'/'+model_gqs[count]+'/frbx_'+res_keys[count]+'_500kpc_'+redshift_keys[count]+'_Temperature_Densityweight.cpkl'
+		tempname = temp_beg+redshift_keys[count]+'/'+model_gqs[count]+'/frbx_'+res_keys[count]+'_500kpc_'+redshift_keys[count]+'_Temperature_Density.cpkl'
+
+		#print tempname
 
 		fractions = find_covering_fraction(modelnames[0],tempname,SB_lims,znow[count])
 		lim1 = np.append(lim1,fractions[0])
 		lim2 = np.append(lim2,fractions[1])
 		lim3 = np.append(lim3,fractions[2])	
-		zplot = np.append(zplot,znow[count])
 		count = count + 1
 
 	ax[0].plot(zplot,lim1,'-o',color=colors[i],label=ion)
@@ -73,7 +74,7 @@ for ion in ions:
 ax[0].set_title('SB > 1')
 ax[1].set_title('SB > 2')
 ax[2].set_title('SB > 3')
-ax[0].set_ylabel('log(SB) [photons s^-1 cm^-2 sr^-1]')
+ax[0].set_ylabel('Fraction of Pixels')
 ax[1].set_xlabel('Redshift')
 ax[2].legend()
 plt.tight_layout()
