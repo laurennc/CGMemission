2d01f69ba7f51b95ccf3e6b1b4b3a5d541637b5d
laurennc <lcorlie@gmail.com>
2016-02-22 11:59:23 -0500

    added option to plot on the brightest emission pixels

--- explore_ion_ratio.py
+++ explore_ion_ratio.py
@@ -34,65 +34,77 @@ def basic_plot(ax,xfield,yfield1,yfield2):
 	ax.set_xticks(np.arange(round(np.log10(xfield).min(),1), round(np.log10(xfield).max()+1,1), 1.0))
 	return
 
-def basic_imshow(ax,xfield,yfield1,yfield2):
-	ion_frac = yfield1/yfield2
-	ion_frac = np.log10(ion_frac.flat)
-	#hist,xedges,yedges = np.histogram2d(np.log10(xfield.flat),ion_frac,range=[[4,6.5],[-3,3]],bins=100)
-	#hist,xedges,yedges = np.histogram2d(np.log10(xfield.flat),ion_frac,range=[[-5,-1],[-4,2]],bins=100)
-	hist,xedges,yedges = np.histogram2d(np.log10(xfield.flat),ion_frac,range=[[18,22],[-6,3]],bins=100)
-
-	extent = [yedges[0], yedges[-1], xedges[0], xedges[-1]]
-	#ax.imshow(np.log10(hist.T),origin='lower',extent=[4,6.5,-3,3],interpolation='nearest',cmap='jet',vmin=-3.,vmax=3.) 
-	#ax.imshow(np.log10(hist.T),origin='lower',extent=[-5,-1,-4,2],interpolation='nearest',cmap='jet',vmin=-3.,vmax=3.)
-	ax.imshow(np.log10(hist.T),origin='lower',extent=[18,22,-6,3],interpolation='nearest',cmap='jet',vmin=-3.,vmax=3.)
-	#ax.set_xlim(4,6.5)
+def basic_imshow(ax,xfield,yfield1,yfield2,emis=False,plot_type='temp'):
+	if emis:
+		yfield1,yfield2,xfield = yfield1.flat,yfield2.flat,xfield.flat
+		idx1 = np.where(yfield1 > 1.)[0]
+		yfield1,yfield2,xfield = yfield1[idx1],yfield2[idx1],xfield[idx1]
+		idx2 = np.where(yfield2 > 1.)[0]
+		yfield1,yfield2,xfield = yfield1[idx2],yfield2[idx2],xfield[idx2]
+		ion_frac = np.log10(yfield1/yfield2)
+	else:
+		ion_frac = yfield1/yfield2
+		xfield = xfield.flat
+		ion_frac = np.log10(ion_frac.flat)
+	if plot_type == 'temp':
+		hist,xedges,yedges = np.histogram2d(np.log10(xfield),ion_frac,range=[[4,6.5],[-3,3]],bins=100)
+		extent = [yedges[0], yedges[-1], xedges[0], xedges[-1]]
+        	ax.imshow(np.log10(hist.T),origin='lower',extent=[4,6.5,-3,3],interpolation='nearest',cmap='jet',vmin=-3.,vmax=3.) 
+	if plot_type == 'dens':
+		hist,xedges,yedges = np.histogram2d(np.log10(xfield),ion_frac,range=[[-5,-1],[-4,2]],bins=100)
+		extent = [yedges[0], yedges[-1], xedges[0], xedges[-1]]
+        	ax.imshow(np.log10(hist.T),origin='lower',extent=[-5,-1,-4,2],interpolation='nearest',cmap='jet',vmin=-3.,vmax=3.)
+	if plot_type == 'NH':
+		hist,xedges,yedges = np.histogram2d(np.log10(xfield),ion_frac,range=[[18,22],[-6,3]],bins=100)
+		extent = [yedges[0], yedges[-1], xedges[0], xedges[-1]]
+		ax.imshow(np.log10(hist.T),origin='lower',extent=[18,22,-6,3],interpolation='nearest',cmap='jet',vmin=-3.,vmax=3.)
 	ax.set_aspect('auto')
 	return
 
 
-xlen,ylen = 3,2 #6#2,6
-fig,ax = plt.subplots(ylen,xlen,sharex=True,sharey=True)
-fig.set_size_inches(9,6)
-ax = ax.flat
-
-fileout = 'NH_CIII_OVI_imshow.png'
-#xfield = 'Temperature_Density'
-xfield = 'H_NumberDensity'
-#xfield = 'Density'
-basic = load_basic_file(xfield)
+fields = ['Temperature_Density','H_NumberDensity','Density']
+key = ['temp','NH','dens']
 
 model_gqs = ['g1q01','g1q1','g1q10']
-ion1,ion2,ion3 = 'CIII_977','OVI','CIII'
-#ion1,ion2,ion3 = 'SiIII','SiIII','SiII'
+ion1,ion2,ion3 = 'CIV','OVI','CIV' ##the first is CIII_977
 count = 0
+j = 0
 
-for i in range(len(ax)):
-	print i,count
-	if i < 3:
-		ion_top = load_emis_file(ion1,model_gqs[count])
-		ion_btm = load_emis_file(ion2,model_gqs[count])
-		#basic_plot(ax[i],basic,ion_top,ion_btm)
-		basic_imshow(ax[i],basic,ion_top,ion_btm)
-		ax[i].set_title(model_gqs[i])
-	else:
-		ion_top = load_coldens_file(ion3,model_gqs[count])
-		ion_btm = load_coldens_file(ion2,model_gqs[count])
-		#basic_plot(ax[i],basic,ion_top,ion_btm)
-		basic_imshow(ax[i],basic,ion_top,ion_btm)		
+for field in fields:
+	fileout = key[j]+'_'+ion3+'_'+ion2+'_imshow_brightest.png'
+	basic = load_basic_file(field)
 
-	count = count + 1
-	if count == 3:
-		count = 0
+	xlen,ylen = 3,2 #6#2,6
+	fig,ax = plt.subplots(ylen,xlen,sharex=True,sharey=True)
+	fig.set_size_inches(9,6)
+	ax = ax.flat
 
+	for i in range(len(ax)):
+		print i,count
+		if i < 3:
+			ion_top = load_emis_file(ion1,model_gqs[count])
+			ion_btm = load_emis_file(ion2,model_gqs[count])
+			#basic_plot(ax[i],basic,ion_top,ion_btm)
+			basic_imshow(ax[i],basic,ion_top,ion_btm,emis=True,plot_type=key[j])
+			#basic_imshow(ax[i],basic,ion_top,ion_btm)
+			ax[i].set_title(model_gqs[i])
+		else:
+			ion_top = load_coldens_file(ion3,model_gqs[count])
+			ion_btm = load_coldens_file(ion2,model_gqs[count])
+			#basic_plot(ax[i],basic,ion_top,ion_btm)
+			basic_imshow(ax[i],basic,ion_top,ion_btm,emis=False,plot_type=key[j])
+	
+		count = count + 1
+		if count == 3:
+			count = 0
+	
 
-ax[0].set_ylabel('Emission '+ion1+'/'+ion2)
-ax[3].set_ylabel('Absorption '+ion3+'/'+ion2)
-ax[4].set_xlabel('N_H')
-#ax[4].set_xlabel('Temperature')
-#ax[4].set_xlabel('Density')
-plt.savefig(fileout)
-plt.close()
+	ax[0].set_ylabel('Emission '+ion1+'/'+ion2)
+	ax[3].set_ylabel('Absorption '+ion3+'/'+ion2)
+	ax[4].set_xlabel(field)
+	plt.savefig(fileout)
+	plt.close()
 
-
+	j = j + 1
 
 
